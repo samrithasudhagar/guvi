f3e2f8364c93f06f5343abcaee92e47293168996
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-01-07 09:29:30 +0000

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,5 @@
 s=input()
 if s=="a" or s=="e" or s=="i" or s=="o" or s=="u":
-  print("Vowel")https://github.com/samrithasudhagar/guvi/blob/master/3.py
+  print("Vowel")
 else:
   print("Consonant")
