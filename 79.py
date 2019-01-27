21e6c8e428202546f5c5a7328910ec5b4045ee3b
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-01-27 20:42:32 +0530

    Update 79.py

--- 79.py
+++ 79.py
@@ -2,11 +2,14 @@ n,m=map(int,input().split())
 a=n*m
 for i in range(1,1000):
 	if (i*i)==a:
-		print("yes")
 		flag=0
 		break
 	else:
 		flag=1
-if flag!=0:
+if a==0:
+	print("yes")
+elif flag!=0:
 	print("no")
-  #i
+elif flag==0:
+	print("yes")
+	#i
