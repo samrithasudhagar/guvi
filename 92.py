d1d47b65f0eb787f6bbc4109f96392b90408c8f2
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-01-30 16:15:09 +0530

    Update 92.py

--- 92.py
+++ 92.py
@@ -1,10 +1,3 @@
 n=int(input())
-l=[]
-sum=0
-for i in range(0,n):
-	m=int(input())
-	l.append(m)
-for i in range(0,len(l)):
-	sum=sum+l[i]
-print(sum)
-#i
+l=list(map(int,input().split()))
+print(sum(l))
